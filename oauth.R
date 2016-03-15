5fe3ffd11bf4a83390f0702743e4a63af8e9ff26
bilydr <bilongyi@gmail.com>
2016-03-15 16:27:55 -0400

    update .gitignore

--- oauth.R
+++ oauth.R
@@ -1,7 +1,7 @@
 library(twitteR)
-api_key <- "xfhEel0vQt8jgqB9StemGPOUa"
+api_key <- "aQ10nUG7ejTxcBTSg1yUvechX"
 
-api_secret <- "0vhe9Nwm3GbKt6oGJU7cRGriewr0L1E2nCIBKoE3PNBieIDVJr"
+api_secret <- "navn3S2P34a3Q1Eavm0kLXYOvZG9l53tN6JM6tueFeigECufRa"
 
 access_token <- "3413377167-WUMwzmOcB9oY7hNumMRV3884v8WKnYpaII5enOC"
 
